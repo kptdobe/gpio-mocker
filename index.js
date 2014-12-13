6b5ac6d4c20803f44ca50d3ecb48bb737e427fa1
Alexandre Capt <acapt@adobe.com>
2014-12-13 11:38:23 +0100

    Clean up

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-
-
-module.exports = function(options) {
+module.exports = function (options) {
     var mocker = require('./lib/gpio-mocker')(options);
     require('./lib/DS18B20-sensor')(mocker);
 
--- sample.js
+++ sample.js
@@ -2,18 +2,18 @@
 var mocker = require('./index')({path: __dirname + '/mocks'});
 
 //write 1 on pin 4
-mocker.write(4, 1, function(err) {
-    if ( err != null ) {
-        console.log('Error',err);
+mocker.write(4, 1, function (err) {
+    if (err != null) {
+        console.log('Error', err);
     } else {
         console.log('Successfully wrote 1 to pin 4');
     }
 });
 
 //write a temperature to simulate a DS18B20 sensor
-mocker.w1.write('mydevice', 'DS18B20', 23, function(err) {
-    if ( err != null ) {
-        console.log('Error',err);
+mocker.w1.write('mydevice', 'DS18B20', 23, function (err) {
+    if (err != null) {
+        console.log('Error', err);
     } else {
         console.log('Successfully wrote to mydevice');
     }
